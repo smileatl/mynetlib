575058feacf145ee4e1803d9531cd2ed6a622103
lsl <1355484300@qq.com>
2023-08-10 12:01:03 +0800

    change

--- include/Timestamp.h
+++ include/Timestamp.h
@@ -16,6 +16,9 @@ public:
     std::string toString() const;
     std::string toFormattedString(bool showMicroseconds = true) const;
 
+    //返回当前时间戳的微妙
+    int64_t microSecondsSinceEpoch() const { return microSecondsSinceEpoch_; }
+
     void swap(Timestamp& that) {
         std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_);
     }
@@ -24,5 +27,14 @@ public:
 private:
     int64_t microSecondsSinceEpoch_;
 };
+
+// 如果是重复定时任务就会对此时间戳进行增加。
+inline Timestamp addTime(Timestamp timestamp, double seconds)
+{
+    // 将延时的秒数转换为微妙
+    int64_t delta = static_cast<int64_t>(seconds * Timestamp::kMicroSecondsPerSecond);
+    // 返回新增时后的时间戳
+    return Timestamp(timestamp.microSecondsSinceEpoch() + delta);
+}
 
 }
