86ad8ad6a5d30053a4b214c555d1708b2e69ff27
lsl <1355484300@qq.com>
2023-08-21 09:46:18 +0800

    change

---
+++ example/echo/build/CMakeFiles/echotest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echotest.dir/testserver.o"
+  "echotest"
+  "echotest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/echotest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- example/echo/testserver.cc
+++ example/echo/testserver.cc
@@ -3,6 +3,8 @@
 #include <functional>
 #include <string>
 
+using namespace mynetlib;
+
 class EchoServer {
 public:
     EchoServer(EventLoop* loop,
@@ -14,7 +16,7 @@ public:
         server_.setConnectionCallback(
             std::bind(&EchoServer::onConnection, this, std::placeholders::_1));
 
-        //将用户定义的可读事件处理函数注册进TcpServer中，TcpServer发生可读事件时会执行onMessage函数。
+        // 将用户定义的可读事件处理函数注册进TcpServer中，TcpServer发生可读事件时会执行onMessage函数。
         server_.setMessageCallback(
             std::bind(&EchoServer::onMessage, this, std::placeholders::_1,
                       std::placeholders::_2, std::placeholders::_3));
@@ -27,8 +29,11 @@ public:
 
 private:
     // 连接建立或者断开的回调
-    // 用户定义的连接事件处理函数：当服务端接收到新连接建立请求，则打印Connection UP，如果是关闭连接请求，则打印Connection Down
+    // 用户定义的连接事件处理函数：当服务端接收到新连接建立请求，则打印Connection
+    // UP，如果是关闭连接请求，则打印Connection Down
+    // conn参数是TcpConnection对象的shared_ptr
     void onConnection(const TcpConnectionPtr& conn) {
+        // conn->connected()返回一个bool值，表明目前连接是建立还是断开
         if (conn->connected()) {
             LOG_INFO("Connection UP : %s",
                      conn->peerAddress().toIpPort().c_str());
@@ -39,6 +44,10 @@ private:
     }
 
     // 可读写事件回调
+    // conn参数是收到数据的那个TCP连接
+    // buf是已经收到的数据，buf的数据会累积，直到用户从中取走（retrieve）数据
+    // buf是指针，表明用户代码可以修改（消费）buffer
+    // time是收到数据的确切时间，即epoll_wait(2)返回的时间，注意这个时间通常比read(2)发生的时间略早，可以用于正确测量程序的消息处理延迟
     void onMessage(const TcpConnectionPtr& conn, Buffer* buf, Timestamp time) {
         // 服务器获取客户端发送的数据
         std::string msg = buf->retrieveAllAsString();
@@ -53,10 +62,11 @@ private:
 };
 
 int main() {
-    //这个EventLoop就是main EventLoop，即负责循环事件监听处理新用户连接事件的事件循环器。
+    // 这个EventLoop就是main
+    // EventLoop，即负责循环事件监听处理新用户连接事件的事件循环器。
     EventLoop loop;
 
-    //InetAddress其实是对socket编程中的sockaddr_in进行封装，使其变为更友好简单的接口而已。
+    // InetAddress其实是对socket编程中的sockaddr_in进行封装，使其变为更友好简单的接口而已。
     InetAddress addr(6000);
 
     // 创建了Acceptor对象 non-blocking listenfd create bind
@@ -69,3 +79,5 @@ int main() {
 
     return 0;
 }
+
+
